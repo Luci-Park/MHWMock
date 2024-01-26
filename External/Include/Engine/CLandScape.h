b2dddc35a0e72b773109e8a2cc0149bce308e41f
lurufarm <hsg200528@gmail.com>
2024-01-26 18:10:47 +0900

    Revert "Revert "05_LandScapeUI""
    This reverts commit c1397626ed763889d902a985ef560f53bce5a3be.

--- External/Include/Engine/CLandScape.h
+++ External/Include/Engine/CLandScape.h
@@ -15,6 +15,10 @@ public:
 	void SetFace(UINT _iFaceX, UINT _iFaceZ);
 	void SetHeightMap(Ptr<CTexture> _HeightMap) { m_HeightMap = _HeightMap; }
 
+	int GetFaceX() { return m_iFaceX; }
+	int GetFaceZ() { return m_iFaceZ; }
+	Ptr<CTexture> GetHeightMap() { return m_HeightMap; }
+
 	virtual void finaltick() override;
 	virtual void render() override;
 
