235c301f222dc77cd7565ee5038bc2aaf08d2239
Mooooon-s <mmk990212@naver.com>
2024-01-28 13:36:30 +0900

    Gizmo move to TransformUI

--- Project/Client/ImGuiMgr.cpp
+++ Project/Client/ImGuiMgr.cpp
@@ -1,6 +1,5 @@
 #include "pch.h"
 #include "ImGuiMgr.h" 
-#include "ImGuizmo.h"
 
 #include <Engine\CDevice.h>
 #include <Engine\CLevelMgr.h>
@@ -15,6 +14,7 @@
 #include "Engine\CCamera.h"
 #include "Engine\CTransform.h"
 #include "OutlinerUI.h"
+
 
 ImGuiMgr::ImGuiMgr()
     : m_hMainHwnd(nullptr)   
@@ -92,7 +92,6 @@ void ImGuiMgr::progress()
     
     tick();
     finaltick();
-    renderGizmo();
 
     render();
 
@@ -107,14 +106,6 @@ void ImGuiMgr::begin()
     ImGui_ImplDX11_NewFrame();
     ImGui_ImplWin32_NewFrame();
     ImGui::NewFrame();
-
-    float x = ImGui::GetMainViewport()->WorkPos.x;
-    float y = ImGui::GetMainViewport()->WorkPos.y;
-    float windowWidth = ImGui::GetMainViewport()->WorkSize.x;
-    float windowHeight = ImGui::GetMainViewport()->WorkSize.y;
-
-    ImGuizmo::SetOrthographic(false);
-    ImGuizmo::SetRect(x, y, windowWidth, windowHeight);
 
     ParamUI::g_NextId = 0;
 
@@ -146,85 +137,6 @@ void ImGuiMgr::finaltick()
         ImGui::SetWindowFocus(nullptr);
 
 
-}
-
-void ImGuiMgr::renderGizmo()
-{
-
-    static ImGuizmo::MODE mCurrentGizmoMode(ImGuizmo::LOCAL);
-    static ImGuizmo::OPERATION mCurrentGizmoOperation = ImGuizmo::TRANSLATE;
-
-    //Gizmo
-
-    //GetTarget
-    OutlinerUI* ui = (OutlinerUI*)FindUI("##Outliner");
-    CGameObject* selectedObj = ui->GetSelectedObject();
-
-    if (nullptr != selectedObj)
-    {
-        ImGui::Begin("Gizmo");
-        //ImGuizmo::SetImGuiContext(ImGui::GetCurrentContext());
-        ImGuizmo::AllowAxisFlip(false);
-        ImGuizmo::SetDrawlist(ImGui::GetBackgroundDrawList());
-
-        //GetCamera Matrix
-        CCamera* editorCamera = CRenderMgr::GetInst()->GetEditorCamera();
-        XMMATRIX viewMat = editorCamera->GetViewMat();
-        XMMATRIX projMat = editorCamera->GetProjMat();
-
-        XMFLOAT4X4 v = change_mat(viewMat);
-        XMFLOAT4X4 p = change_mat(projMat);
-
-        v.m[0][0] = -v.m[0][0];
-        v.m[1][0] = -v.m[1][0];
-        v.m[2][0] = -v.m[2][0];
-        //
-        v.m[0][1] = -v.m[0][1];
-        v.m[1][1] = -v.m[1][1];
-        v.m[2][1] = -v.m[2][1];
-        //
-        v.m[0][2] = -v.m[0][2];
-        v.m[1][2] = -v.m[1][2];
-        v.m[2][2] = -v.m[2][2];
-
-        //Get ojectTransformMatrix
-        auto objTransform = selectedObj->GetComponent(COMPONENT_TYPE::TRANSFORM)->Transform();
-        XMMATRIX objMat = objTransform->GetWorldMat();
-        XMMATRIX objMatInv = objTransform->GetWorldInvMat();
-        DirectX::XMFLOAT4X4 w = change_mat(objMat*objMatInv);
-
-        ImGuiIO& io = ImGui::GetIO();
-
-        ImGui::Text(ImGuizmo::IsOver() ? "Over gizmo" : "not over");
-        ImGui::SameLine();
-        ImGui::Text(ImGuizmo::IsOver(ImGuizmo::TRANSLATE) ? "Over translate gizmo" : "");
-        ImGui::Text("X: %f Y: %f", io.MousePos.x, io.MousePos.y);
-
-        if (ImGui::RadioButton("Local", mCurrentGizmoMode == ImGuizmo::LOCAL))
-            mCurrentGizmoMode = ImGuizmo::LOCAL;
-        ImGui::SameLine();
-        if (ImGui::RadioButton("World", mCurrentGizmoMode == ImGuizmo::WORLD))
-            mCurrentGizmoMode = ImGuizmo::WORLD;
-
-        if (ImGui::RadioButton("Translate", mCurrentGizmoOperation == ImGuizmo::TRANSLATE))
-            mCurrentGizmoOperation = ImGuizmo::TRANSLATE;
-        ImGui::SameLine();
-        if (ImGui::RadioButton("Rotate", mCurrentGizmoOperation == ImGuizmo::ROTATE))
-            mCurrentGizmoOperation = ImGuizmo::ROTATE;
-        ImGui::SameLine();
-        if (ImGui::RadioButton("Scale", mCurrentGizmoOperation == ImGuizmo::SCALE))
-            mCurrentGizmoOperation = ImGuizmo::SCALE;
-
-        ImGuizmo::Manipulate(*v.m, *p.m,mCurrentGizmoOperation, mCurrentGizmoMode, *w.m);
-
-        if (ImGuizmo::IsOver())
-        {
-            ImGui::Text("Pos : X: %f Y: %f", ImGui::GetMousePos().x, ImGui::GetMousePos().y);
-            int i = 0;
-        }
-
-        ImGui::End();
-    }
 }
 
 void ImGuiMgr::render()
