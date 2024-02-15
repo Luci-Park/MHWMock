79aba0339bd3c1e8f243bd566fe785bf060ad928
luciPark <lucipark05@gmail.com>
2024-02-15 17:44:05 +0900

    Selection Window

--- Project/Client/AnimatorGraphEditorWindow.cpp
+++ Project/Client/AnimatorGraphEditorWindow.cpp
@@ -24,8 +24,8 @@ void AnimatorGraphEditorWindow::OnDraw()
 	ed::LinkId contextLinkId = 0;
 	ed::PinId  contextPinId = 0;
 	ed::SetCurrentEditor(m_pEditor);
-	Splitter(true, 4.0f, &m_fLeftPaneWidth, &m_fRightPaneWidth, 50.0f, 50.0f);
-	ShowLeftPlane(m_fLeftPaneWidth - 4.0f);
+	Splitter(true, 4.0f, &m_fLeftPlaneWidth, &m_fRightPlaneWidth, 50.0f, 50.0f, 0);
+	ShowLeftPanel(m_fLeftPlaneWidth - 4.0f);
 
 	ImGui::SameLine(0.0f, 12.0f);
 	ed::Begin("Node Editor");
@@ -51,17 +51,60 @@ void AnimatorGraphEditorWindow::OnDraw()
 	ed::End();
 }
 
-void AnimatorGraphEditorWindow::OnStart()
+void AnimatorGraphEditorWindow::ShowLeftPanel(float _width)
 {
-	m_pEditor = ed::CreateEditor();
-	ed::SetCurrentEditor(m_pEditor);
+	ImGui::BeginChild("Left Panel", ImVec2(_width, 0), false, ImGuiWindowFlags_NoScrollbar);
+	float fullHeight = ImGui::GetContentRegionAvail().y;
+	float halfHeight1 = fullHeight * 0.5f;
+	float halfHeight2 = fullHeight * 0.5f;
+	Splitter(false, 4.0f, &halfHeight1, &halfHeight2, 50.0f, 50.0f, 0);
+	ShowSelection(_width, halfHeight1 - 5.2f);
+	ImGui::Dummy(ImVec2(0, 5.f));
+	ShowParamConfigPanel(_width, halfHeight2 - 5.2f);
+
+	ImGui::EndChild();
 }
 
-void AnimatorGraphEditorWindow::ShowLeftPlane(float _width)
+void AnimatorGraphEditorWindow::ShowSelection(float _width, float _height)
 {
-	ImGui::BeginChild("Params", ImVec2(_width, 0));
+	ImGui::BeginChild("Selection", ImVec2(_width, _height));
 	_width = ImGui::GetContentRegionAvail().x;
-	
+
+#pragma region Header
+	ImGui::GetWindowDrawList()->AddRectFilled(
+		ImGui::GetCursorScreenPos(),
+		ImGui::GetCursorScreenPos() + ImVec2(_width, ImGui::GetTextLineHeight() * 1.3),
+		ImColor(ImGui::GetStyle().Colors[ImGuiCol_HeaderActive]), ImGui::GetTextLineHeight() * 0.25f);
+	ImGui::Spacing(); ImGui::SameLine();
+	ImGui::TextUnformatted("Selection");
+	ImGui::Dummy(ImVec2(0, 2.f));
+	ImGui::Separator();
+#pragma endregion
+
+	int i = 4;
+	while (i--)
+	{
+		ImGui::Text("Test");
+	}
+	ImGui::EndChild();
+}
+
+void AnimatorGraphEditorWindow::ShowParamConfigPanel(float _width, float _height)
+{
+	ImGui::BeginChild("Params", ImVec2(_width, _height));
+	_width = ImGui::GetContentRegionAvail().x;
+
+#pragma region Header
+	ImGui::GetWindowDrawList()->AddRectFilled(
+		ImGui::GetCursorScreenPos(),
+		ImGui::GetCursorScreenPos() + ImVec2(_width, ImGui::GetTextLineHeight() * 1.3),
+		ImColor(ImGui::GetStyle().Colors[ImGuiCol_HeaderActive]), ImGui::GetTextLineHeight() * 0.25f);
+	ImGui::Spacing(); ImGui::SameLine();
+	ImGui::TextUnformatted("Params");
+	ImGui::Dummy(ImVec2(0, 2.f));
+	ImGui::Separator();
+#pragma endregion
+
 	if (ImGui::Button("Add Param"))
 	{
 		ImGui::OpenPopup("##ParamTypePopup");
@@ -145,6 +188,12 @@ void AnimatorGraphEditorWindow::ShowLeftPlane(float _width)
 	ImGui::EndChild();
 }
 
+void AnimatorGraphEditorWindow::OnStart()
+{
+	m_pEditor = ed::CreateEditor();
+	ed::SetCurrentEditor(m_pEditor);
+}
+
 void AnimatorGraphEditorWindow::OnEnd()
 {
 	if (m_pEditor)
@@ -155,12 +204,14 @@ void AnimatorGraphEditorWindow::OnEnd()
 }
 
 
-bool AnimatorGraphEditorWindow::Splitter(bool split_vertically, float thickness, float* size1, float* size2, float min_size1, float min_size2, float splitter_long_axis_size)
+
+bool AnimatorGraphEditorWindow::Splitter(bool split_vertically, float thickness, float* size1, float* size2, float min_size1, float min_size2, int _id, float splitter_long_axis_size)
 {
 	using namespace ImGui;
 	ImGuiContext& g = *GImGui;
 	ImGuiWindow* window = g.CurrentWindow;
-	ImGuiID id = window->GetID("##Splitter");
+	string name = "##Splitter" + std::to_string(_id);
+	ImGuiID id = window->GetID(name.c_str());
 	ImRect bb;
 	bb.Min = window->DC.CursorPos + (split_vertically ? ImVec2(*size1, 0.0f) : ImVec2(0.0f, *size1));
 	bb.Max = bb.Min + CalcItemSize(split_vertically ? ImVec2(thickness, splitter_long_axis_size) : ImVec2(splitter_long_axis_size, thickness), 0.0f, 0.0f);
--- Project/Client/AnimatorGraphEditorWindow.h
+++ Project/Client/AnimatorGraphEditorWindow.h
@@ -17,16 +17,18 @@ private:
 	CAnimationStateMachine* m_pAnimator;
 	ed::EditorContext* m_pEditor;
 
-	float m_fLeftPaneWidth = 200.0f;
-	float m_fRightPaneWidth = 800.0f;
+	float m_fLeftPlaneWidth = 200.0f;
+	float m_fRightPlaneWidth = 800.0f;
 	int m_iCurrentEditingParam;
 public:
 	virtual void OnDraw();
 private:
 	void OnStart();
 	void OnEnd();
-	void ShowLeftPlane(float _width);
-	bool Splitter(bool split_vertically, float thickness, float* size1, float* size2, float min_size1, float min_size2, float splitter_long_axis_size = -1.0f);
+	void ShowLeftPanel(float _width);
+	void ShowSelection(float _width, float _height);
+	void ShowParamConfigPanel(float _width, float _height);
+	bool Splitter(bool split_vertically, float thickness, float* size1, float* size2, float min_size1, float min_size2, int _id, float splitter_long_axis_size = -1.0f);
 public:
 	AnimatorGraphEditorWindow(CAnimator3D* _animator);
 	~AnimatorGraphEditorWindow();
