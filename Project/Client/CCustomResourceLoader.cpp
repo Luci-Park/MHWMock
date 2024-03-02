d06cb0a399627912efbb26d78c2ec5ed9eb3cbf3
Luci-Park <97658764+Luci-Park@users.noreply.github.com>
2024-03-02 21:58:34 +0900

    Update CCustomResourceLoader.cpp

--- Project/Client/CCustomResourceLoader.cpp
+++ Project/Client/CCustomResourceLoader.cpp
@@ -37,8 +37,8 @@ void LoadCustomResources()
 {
 	LoadCustomShader();
 
-	CModel::LoadFromFbx(L"model\\anjanath_tail.fbx");
+	//CModel::LoadFromFbx(L"model\\anjanath_tail.fbx");
 	//CModel::LoadFromFbx(L"model\\anjanath.fbx");
-	//CModel::LoadFromFbx(L"model\\Character Running.fbx");
+	CModel::LoadFromFbx(L"model\\Character Running.fbx");
 	//CModel::LoadFromFbx(L"model\\Monster.fbx");
 }
