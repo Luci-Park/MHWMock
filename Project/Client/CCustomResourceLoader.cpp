fbb16adf274548e2629f0b8893305ff46173032f
Luci-Park <97658764+Luci-Park@users.noreply.github.com>
2024-03-02 18:27:12 +0900

    Uv issue 하나 제거

--- Project/Client/CCustomResourceLoader.cpp
+++ Project/Client/CCustomResourceLoader.cpp
@@ -37,7 +37,7 @@ void LoadCustomResources()
 {
 	LoadCustomShader();
 
-	//CModel::LoadFromFbx(L"model\\anjanath_tail.fbx");
+	CModel::LoadFromFbx(L"model\\anjanath_tail.fbx");
 	//CModel::LoadFromFbx(L"model\\anjanath.fbx");
 	//CModel::LoadFromFbx(L"model\\Character Running.fbx");
 	//CModel::LoadFromFbx(L"model\\Monster.fbx");
