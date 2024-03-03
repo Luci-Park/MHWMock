8edc3a4f0410d1cb6a7e31e933aa3189774c4e9e
Luci-Park <97658764+Luci-Park@users.noreply.github.com>
2024-03-03 19:28:02 +0900

    Header

--- Project/Engine/CGraphicsShader.cpp
+++ Project/Engine/CGraphicsShader.cpp
@@ -38,6 +38,8 @@ void CGraphicsShader::CreateVertexShader(const wstring& _strFileName, const stri
 
 
 
+	int count = 0;
+
 	// InputLayout »ý¼º
 	D3D11_INPUT_ELEMENT_DESC LayoutDesc[8] = {};
 
@@ -48,63 +50,70 @@ void CGraphicsShader::CreateVertexShader(const wstring& _strFileName, const stri
 	LayoutDesc[0].InstanceDataStepRate = 0;
 	LayoutDesc[0].AlignedByteOffset = 0;
 	LayoutDesc[0].Format = DXGI_FORMAT_R32G32B32_FLOAT;
-
+	count += 3;
 
 	LayoutDesc[1].SemanticName = "COLOR";
 	LayoutDesc[1].SemanticIndex = 0;
 	LayoutDesc[1].InputSlot = 0;
 	LayoutDesc[1].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
 	LayoutDesc[1].InstanceDataStepRate = 0;
-	LayoutDesc[1].AlignedByteOffset = 12;
+	LayoutDesc[1].AlignedByteOffset = sizeof(float) * count;
 	LayoutDesc[1].Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
+	count += 4;
 
 	LayoutDesc[2].SemanticName = "TEXCOORD";
 	LayoutDesc[2].SemanticIndex = 0;
 	LayoutDesc[2].InputSlot = 0;
 	LayoutDesc[2].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
 	LayoutDesc[2].InstanceDataStepRate = 0;
-	LayoutDesc[2].AlignedByteOffset = 28;
+	LayoutDesc[2].AlignedByteOffset = sizeof(float) * count;;
 	LayoutDesc[2].Format = DXGI_FORMAT_R32G32_FLOAT;
+	count += 2;
 
 	LayoutDesc[3].SemanticName = "NORMAL";
 	LayoutDesc[3].SemanticIndex = 0;
 	LayoutDesc[3].InputSlot = 0;
 	LayoutDesc[3].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
 	LayoutDesc[3].InstanceDataStepRate = 0;
-	LayoutDesc[3].AlignedByteOffset = 36;
+	LayoutDesc[3].AlignedByteOffset = sizeof(float) * count;;
 	LayoutDesc[3].Format = DXGI_FORMAT_R32G32B32_FLOAT;
+	count += 3;
 
 	LayoutDesc[4].SemanticName = "TANGENT";
 	LayoutDesc[4].SemanticIndex = 0;
 	LayoutDesc[4].InputSlot = 0;
 	LayoutDesc[4].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
 	LayoutDesc[4].InstanceDataStepRate = 0;
-	LayoutDesc[4].AlignedByteOffset = 48;
+	LayoutDesc[4].AlignedByteOffset = sizeof(float) * count;;
 	LayoutDesc[4].Format = DXGI_FORMAT_R32G32B32_FLOAT;
+	count += 3;
 
 	LayoutDesc[5].SemanticName = "BINORMAL";
 	LayoutDesc[5].SemanticIndex = 0;
 	LayoutDesc[5].InputSlot = 0;
 	LayoutDesc[5].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
 	LayoutDesc[5].InstanceDataStepRate = 0;
-	LayoutDesc[5].AlignedByteOffset = 60;
+	LayoutDesc[5].AlignedByteOffset = sizeof(float) * count;;
 	LayoutDesc[5].Format = DXGI_FORMAT_R32G32B32_FLOAT;
+	count += 3;
 
 	LayoutDesc[6].SemanticName = "BLENDWEIGHT";
 	LayoutDesc[6].SemanticIndex = 0;
-	LayoutDesc[6].AlignedByteOffset = 72;
+	LayoutDesc[6].AlignedByteOffset = sizeof(float) * count;;
 	LayoutDesc[6].Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
 	LayoutDesc[6].InputSlot = 0;
 	LayoutDesc[6].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
 	LayoutDesc[6].InstanceDataStepRate = 0;
+	count += 4;
 
 	LayoutDesc[7].SemanticName = "BLENDINDICES";
 	LayoutDesc[7].SemanticIndex = 0;
-	LayoutDesc[7].AlignedByteOffset = 88;
+	LayoutDesc[7].AlignedByteOffset = sizeof(float) * count;;
 	LayoutDesc[7].Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
 	LayoutDesc[7].InputSlot = 0;
 	LayoutDesc[7].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
 	LayoutDesc[7].InstanceDataStepRate = 0;
+	count += 4;
 
 	if (FAILED(DEVICE->CreateInputLayout(LayoutDesc, 8
 		, m_VSBlob->GetBufferPointer(), m_VSBlob->GetBufferSize()
