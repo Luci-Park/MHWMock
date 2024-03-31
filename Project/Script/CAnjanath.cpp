047d6244d9a3e94a8b3afb3180bfd9a5b0682bb9
Luci-Park <97658764+Luci-Park@users.noreply.github.com>
2024-03-31 12:48:45 +0900

    Add Player to Script

--- Project/Script/CAnjanath.cpp
+++ Project/Script/CAnjanath.cpp
@@ -1,11 +1,30 @@
 #include "pch.h"
 #include "CAnjanath.h"
+#include <Engine/CAnimator3D.h>
+
 
 CAnjanath::CAnjanath()
-	:CScript(SCRIPT_TYPE::ANJANATH)
+	: CScript(SCRIPT_TYPE::ANJANATH)
+	, m_bAggroed(false)
 {
+	AddScriptParam(SCRIPT_PARAM::GAMEOBJECT, &m_pPlayer, "Player");
 }
 
 CAnjanath::~CAnjanath()
 {
+}
+
+void CAnjanath::begin()
+{
+	Animator3D()->SetBool(L"Aggroed", false);
+}
+
+void CAnjanath::tick()
+{
+	if (!m_bAggroed)return;
+}
+
+void CAnjanath::OnCollisionEnter(CCollider3D* _Other)
+{
+	Animator3D()->SetBool(L"Aggroed", true);
 }
--- Project/Script/CAnjanath.h
+++ Project/Script/CAnjanath.h
@@ -1,15 +1,26 @@
 #pragma once
 #include <Engine/CScript.h>
+
+enum class ANJ_ATTACK
+{
+	WEAK_BITE, BITE, CONTIN_BITE, CLAW, TAIL_SLAM, TAIL_SWEEP, BODY_SLAM, FORWARD, RUSH, FLAME
+};
+
 class CAnjanath :
 	public CScript
 {
 private:
-	int MaxHP;
-	int KOGaugeThreshold;
-	int ExhaustThreshold;
-	int BaseAttack;
+	CGameObject* m_pPlayer;
+	bool m_bAggroed;
+	ANJ_ATTACK m_Attack;
+	
 public:
-	CLONE(CAnjanath)
+	virtual void begin() override;
+	virtual void tick() override;
+	virtual void OnCollisionEnter(CCollider3D* _Other);
+
+public:
+	CLONE(CAnjanath);
 	CAnjanath();
 	~CAnjanath();
 };
